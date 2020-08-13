0d6fb62f90b342525aeb5297cdbc0a13e05089dd
apolloproductionsdj <apolloproductionsdj@gmail.com>
2020-08-12 17:53:50 -0700

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import './App.css';
+import Sidebar from './Sidebar.js';
+
+function App() {
+  return (
+    <div className="app">
+     {/* Sidebar */}
+     <Sidebar />
+
+
+     {/* Feed */}
+
+     {/* Widgets */}
+
+    </div>
+  );
+}
+
+export default App;
---
+++ src/Sidebar.js
@@ -0,0 +1,27 @@
+import React from 'react';
+import './Sidebar.css';
+
+
+function Sidebar() {
+  return (
+    <div className="sidebar">
+
+    {/* Twitter icon */}
+    {/* Twitter icon */}
+    {/* Twitter icon */}
+    {/* Twitter icon */}
+    {/* Twitter icon */}
+    {/* Twitter icon */}
+
+    {/* SidebarOption */}
+
+
+    {/* Button -> Tweet */}
+
+      <h1>I am the sidebar</h1>
+    
+    </div>
+  )
+}
+
+export default Sidebar;
